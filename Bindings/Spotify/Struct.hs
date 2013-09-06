ca18ba257bfde6329091af6ccf6e3a10cb1afb0d
Thomas Dziedzic <gostrc@gmail.com>
2013-09-06 04:22:48 +0000

    commit Struct.hs

---
+++ Bindings/Spotify/Struct.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE EmptyDataDecls #-}
+
+module Bindings.Spotify.Struct (
+    Sp_Session(..)
+  , Sp_Track(..)
+  , Sp_Album(..)
+  , Sp_Artist(..)
+  , Sp_Artistbrowse(..)
+  , Sp_Albumbrowse(..)
+  , Sp_Toplistbrowse(..)
+  , Sp_Search(..)
+  , Sp_Link(..)
+  , Sp_Image(..)
+  , Sp_User(..)
+  , Sp_Playlist(..)
+  , Sp_Playlistcontainer(..)
+  , Sp_Inbox(..)
+) where
+
+data Sp_Session
+data Sp_Track
+data Sp_Album
+data Sp_Artist
+data Sp_Artistbrowse
+data Sp_Albumbrowse
+data Sp_Toplistbrowse
+data Sp_Search
+data Sp_Link
+data Sp_Image
+data Sp_User
+data Sp_Playlist
+data Sp_Playlistcontainer
+data Sp_Inbox
