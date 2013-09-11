56fc03fbc683cb970098c4b9d68cbfc38a8ff699
Thomas Dziedzic <gostrc@gmail.com>
2013-09-11 04:33:54 +0000

    write main process events loop

--- example/login.hs
+++ example/login.hs
@@ -13,21 +13,29 @@ import Foreign.C.Types
 import Foreign.Marshal.Alloc
 import System.IO
 import Foreign.Marshal.Array
+import Control.Concurrent.Broadcast (Broadcast)
+import qualified Control.Concurrent.Broadcast as Broadcast
 
 logged_in_cb :: Ptr Sp_Session -> Sp_Error -> IO ()
-logged_in_cb session_ptr err = error "in logged_in_cb"
+logged_in_cb session_ptr err = do
+  putStrLn $ "in logged_in_cb with err: " ++ show err
+  hFlush stdout
 
 foreign import ccall "wrapper"
   mkLoggedInCb :: (Ptr Sp_Session -> Sp_Error -> IO ()) -> IO (FunPtr (Ptr Sp_Session -> Sp_Error -> IO ()))
 
 logged_out_cb :: Ptr Sp_Session -> IO ()
-logged_out_cb session_ptr = error "in logged_out_cb"
+logged_out_cb session_ptr = do
+  putStrLn "in logged_out_cb"
+  hFlush stdout
 
 foreign import ccall "wrapper"
   mkLoggedOutCb :: (Ptr Sp_Session -> IO ()) -> IO (FunPtr (Ptr Sp_Session -> IO ()))
 
 metadata_updated_cb :: Ptr Sp_Session -> IO ()
-metadata_updated_cb session_ptr = error "in metadata_updated_cb"
+metadata_updated_cb session_ptr = do
+  putStrLn "in metadata_updated_cb"
+  hFlush stdout
 
 foreign import ccall "wrapper"
   mkMetadataUpdatedCb :: (Ptr Sp_Session -> IO ()) -> IO (FunPtr (Ptr Sp_Session -> IO ()))
@@ -48,8 +56,10 @@ message_to_user_cb session_ptr msg = do
 foreign import ccall "wrapper"
   mkMessageToUserCb :: (Ptr Sp_Session -> CString -> IO ()) -> IO (FunPtr (Ptr Sp_Session -> CString -> IO ()))
 
-notify_main_thread_cb :: Ptr Sp_Session -> IO ()
-notify_main_thread_cb session_ptr = do
+notify_main_thread_cb :: Broadcast () -> Ptr Sp_Session -> IO ()
+notify_main_thread_cb process_events_broadcast session_ptr = do
+  Broadcast.signal process_events_broadcast ()
+
   putStrLn "in notify_main_thread callback"
   hFlush stdout
 
@@ -60,7 +70,7 @@ type MusicDeliveryCb = Ptr Sp_Session -> Ptr Sp_AudioFormat -> Ptr () -> CInt ->
 
 music_delivery_cb :: MusicDeliveryCb
 music_delivery_cb session_ptr audioformat_ptr frames_ptr num_frames = do
-  putStrLn "in notify_main_thread callback"
+  putStrLn "in music_delivery_cb callback"
   hFlush stdout
   return 1
 
@@ -69,7 +79,7 @@ foreign import ccall "wrapper"
 
 play_token_lost_cb :: Ptr Sp_Session -> IO ()
 play_token_lost_cb session_ptr = do
-  putStrLn "in notify_main_thread callback"
+  putStrLn "in play_token_lost_cb callback"
   hFlush stdout
 
 foreign import ccall "wrapper"
@@ -77,7 +87,9 @@ foreign import ccall "wrapper"
 
 log_message_cb :: Ptr Sp_Session -> CString -> IO ()
 log_message_cb session_ptr log_data = do
-  putStrLn "in notify_main_thread callback"
+  putStrLn "in log_message_cb"
+  logString <- peekCString log_data
+  putStrLn $ "log data: " ++ logString
   hFlush stdout
 
 foreign import ccall "wrapper"
@@ -85,7 +97,7 @@ foreign import ccall "wrapper"
 
 end_of_track_cb :: Ptr Sp_Session -> IO ()
 end_of_track_cb session_ptr = do
-  putStrLn "in notify_main_thread callback"
+  putStrLn "in end_of_track_cb callback"
   hFlush stdout
 
 foreign import ccall "wrapper"
@@ -93,7 +105,7 @@ foreign import ccall "wrapper"
 
 streaming_error_cb :: Ptr Sp_Session -> Sp_Error -> IO ()
 streaming_error_cb session_ptr err = do
-  putStrLn "in connection_error callback"
+  putStrLn "in streaming_error_cb callback"
   hFlush stdout
 
 foreign import ccall "wrapper"
@@ -101,7 +113,7 @@ foreign import ccall "wrapper"
 
 userinfo_updated_cb :: Ptr Sp_Session -> IO ()
 userinfo_updated_cb session_ptr = do
-  putStrLn "in notify_main_thread callback"
+  putStrLn "in userinfo_updated_cb callback"
   hFlush stdout
 
 foreign import ccall "wrapper"
@@ -109,7 +121,7 @@ foreign import ccall "wrapper"
 
 start_playback_cb :: Ptr Sp_Session -> IO ()
 start_playback_cb session_ptr = do
-  putStrLn "in notify_main_thread callback"
+  putStrLn "in start_playback_cb callback"
   hFlush stdout
 
 foreign import ccall "wrapper"
@@ -117,7 +129,7 @@ foreign import ccall "wrapper"
 
 stop_playback_cb :: Ptr Sp_Session -> IO ()
 stop_playback_cb session_ptr = do
-  putStrLn "in notify_main_thread callback"
+  putStrLn "in stop_playback_cb callback"
   hFlush stdout
 
 foreign import ccall "wrapper"
@@ -127,7 +139,7 @@ type GetAudioBufferStatsCb = Ptr Sp_Session -> Ptr Sp_Audio_Buffer_Stats -> IO (
 
 get_audio_buffer_stats_cb :: GetAudioBufferStatsCb
 get_audio_buffer_stats_cb session_ptr audio_buffer_stats_ptr= do
-  putStrLn "in notify_main_thread callback"
+  putStrLn "in get_audio_buffer_stats_cb callback"
   hFlush stdout
 
 foreign import ccall "wrapper"
@@ -135,7 +147,7 @@ foreign import ccall "wrapper"
 
 offline_status_updated_cb :: Ptr Sp_Session -> IO ()
 offline_status_updated_cb session_ptr = do
-  putStrLn "in notify_main_thread callback"
+  putStrLn "in offline_status_updated_cb callback"
   hFlush stdout
 
 foreign import ccall "wrapper"
@@ -143,7 +155,7 @@ foreign import ccall "wrapper"
 
 offline_error_cb :: Ptr Sp_Session -> Sp_Error -> IO ()
 offline_error_cb session_ptr err = do
-  putStrLn "in connection_error callback"
+  putStrLn "in offline_error_cb callback"
   hFlush stdout
 
 foreign import ccall "wrapper"
@@ -151,7 +163,7 @@ foreign import ccall "wrapper"
 
 credentials_blob_updated_cb :: Ptr Sp_Session -> CString -> IO ()
 credentials_blob_updated_cb session_ptr log_data = do
-  putStrLn "in notify_main_thread callback"
+  putStrLn "in credentials_blob_updated_cb callback"
   hFlush stdout
 
 foreign import ccall "wrapper"
@@ -159,7 +171,7 @@ foreign import ccall "wrapper"
 
 connectionstate_updated_cb :: Ptr Sp_Session -> IO ()
 connectionstate_updated_cb session_ptr = do
-  putStrLn "in notify_main_thread callback"
+  putStrLn "in connectionstate_updated_cb callback"
   hFlush stdout
 
 foreign import ccall "wrapper"
@@ -167,7 +179,7 @@ foreign import ccall "wrapper"
 
 scrobble_error_cb :: Ptr Sp_Session -> Sp_Error -> IO ()
 scrobble_error_cb session_ptr err = do
-  putStrLn "in connection_error callback"
+  putStrLn "in scrobble_error_cb callback"
   hFlush stdout
 
 foreign import ccall "wrapper"
@@ -175,7 +187,7 @@ foreign import ccall "wrapper"
 
 private_session_mode_changed_cb :: Ptr Sp_Session -> Sp_Bool -> IO ()
 private_session_mode_changed_cb session_ptr is_private  = do
-  putStrLn "in connection_error callback"
+  putStrLn "in private_session_mode_changed_cb callback"
   hFlush stdout
 
 foreign import ccall "wrapper"
@@ -189,12 +201,14 @@ main = do
   putStrLn "Key Length:"
   putStrLn . show . BS.length $ key
 
+  process_events_broadcast <- Broadcast.new
+
   logged_in_cb_ptr                    <- mkLoggedInCb logged_in_cb
   logged_out_cb_ptr                   <- mkLoggedOutCb logged_out_cb
   metadata_updated_cb_ptr             <- mkMetadataUpdatedCb metadata_updated_cb
   connection_error_cb_ptr             <- mkConnectionErrorCb connection_error_cb
   message_to_user_cb_ptr              <- mkMessageToUserCb message_to_user_cb
-  notify_main_thread_cb_ptr           <- mkNotifyMainThreadCb notify_main_thread_cb
+  notify_main_thread_cb_ptr           <- mkNotifyMainThreadCb $ notify_main_thread_cb process_events_broadcast
   music_delivery_cb_ptr               <- mkMusicDeliveryCb music_delivery_cb
   play_token_lost_cb_ptr              <- mkPlayTokenLostCb play_token_lost_cb
   log_message_cb_ptr                  <- mkLogMessageCb log_message_cb
@@ -293,16 +307,33 @@ main = do
   errMsg <- peekCString <=< c_sp_error_message $ err
   putStrLn . show $ errMsg
 
-  process_events session_ptr
-  --threadDelay 10000000
+  delay_process_events process_events_broadcast session_ptr
 
-process_events :: Ptr Sp_Session -> IO ()
-process_events session_ptr = do
-  putStrLn "processing events"
+delay_process_events :: Broadcast () -> Ptr Sp_Session -> IO ()
+delay_process_events process_events_broadcast session_ptr = do
+  putStrLn "delay_process_events"
+  hFlush stdout
+  Broadcast.listen process_events_broadcast
+  putStrLn "passed"
+  hFlush stdout
 
+  process_events process_events_broadcast session_ptr
+
+delay_process_events':: Broadcast () -> Integer -> Ptr Sp_Session -> IO ()
+delay_process_events' process_events_broadcast timeout session_ptr = do
+  putStrLn "delay_process_events'"
+  hFlush stdout
+  Broadcast.listenTimeout process_events_broadcast timeout
+  putStrLn "passed"
+  hFlush stdout
+
+  process_events process_events_broadcast session_ptr
+
+process_events :: Broadcast () -> Ptr Sp_Session -> IO ()
+process_events process_events_broadcast session_ptr = do
   next_timeout_ptr <- (malloc :: IO (Ptr CInt))
-
   err <- c_sp_session_process_events session_ptr next_timeout_ptr
+
   errMsg <- peekCString <=< c_sp_error_message $ err
   putStrLn . show $ errMsg
 
@@ -310,6 +341,5 @@ process_events session_ptr = do
 
   let delay_us = (1000 * fromIntegral next_timeout_ms)
   putStrLn $ "delaying " ++ show delay_us ++ "ms"
-  threadDelay $ 10 * 1000000
 
-  process_events session_ptr
+  delay_process_events' process_events_broadcast delay_us session_ptr
